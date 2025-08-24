f48097f6b5cdc9e1c734fe40c8f33e40a8abd379
Callum Elliott <callum.elliott.11@hotmail.com>
2025-08-24 07:58:33 +0100

    Fix build script

--- build.rs
+++ build.rs
@@ -1,3 +1,13 @@
 fn main() {
-    embed_resource::compile("resources.rc", embed_resource::NONE);
+    // Only embed resources on Windows
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.set("ProductName", "Image Mover");
+        res.set("FileDescription", "Image Mover Application");
+        res.set("OriginalFilename", "image_mover.exe");
+        res.set("FileVersion", "0.1.0.0");
+        res.compile().unwrap();
+    }
 }
